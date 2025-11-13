2a8637629a7a98af388d50462db20e7d205098e0
Az15 <agustinlozanoxdxd@gmail.com>
2025-11-13 08:49:40 -0300

    Modificaciones visuales. Parte 1. Index, Vuelos, Hoteles

---
+++ WebTurismoTriunvirato/wwwroot/js/fixModalsLayout.js
@@ -0,0 +1,16 @@
+﻿$(document).on('hidden.bs.modal', function (e) {
+    // 1. Revisa si hay otros modales aún abiertos
+    const openModals = document.querySelectorAll('.modal.show');
+
+    // 2. Si NO hay modales abiertos...
+    if (openModals.length === 0) {
+        // ... busca y elimina forzosamente cualquier backdrop restante
+        const backdrop = document.querySelector('.modal-backdrop');
+        if (backdrop) {
+            backdrop.remove();
+        }
+
+        // También asegura que el scroll del cuerpo vuelva a funcionar
+        document.body.style.overflow = '';
+    }
+});
